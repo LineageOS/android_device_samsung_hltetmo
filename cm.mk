f7480e18c8913b6e069756acf165e52417186b02
utkanos <utkanos@gmail.com>
2013-10-02 14:30:39 -0500

    mkvendor.sh: Initial commit of hltetmo

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := hltetmo
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/hltetmo/device_hltetmo.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hltetmo
+PRODUCT_NAME := cm_hltetmo
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := hltetmo
+PRODUCT_MANUFACTURER := samsung
