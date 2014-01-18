130efa3d1b2bbf2b83677496909fd33d3b5c634c
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-18 04:26:38 -0600

    hlte: make tree universal.
    * do what hammerhead did.
    * shove cdma and gsm into one universal tree based on hardware
    revision.
    * this is for nxp nfc
    
    Change-Id: I4f2063ad5296297a945976ca011823c2a0487129

---
+++ variant.sh
@@ -0,0 +1,53 @@
+#!/system/bin/sh
+# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2013-2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Allows customization for various hltenci variants.
+#
+
+
+CMDLINE=`cat /proc/cmdline`
+mkdir /system/tmp
+
+mv /system/build.prop /system/tmp/build.prop
+
+if [[ $CMDLINE != "${CMDLINE/SM-N900T}" ]]; #hltetmo
+    then
+       sed -i 's|ro.build.fingerprint=xxxxxxxxxx|ro.build.fingerprint=samsung/hltetmo/hltetmo:4.3/JSS15J/N900TUVUBMI7:user/release-keys|g' /tmp/build.prop
+       sed -i 's|ro.build.description=xxxxxxxxxx|hltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys|g' /tmp/build.prop
+       sed -i 's|ro.product.model=hlte|ro.product.model=SM-N900T|g' /tmp/build.prop
+       sed -i 's|ro.product.device=hlte|ro.product.device=hltetmo|g' /tmp/build.prop
+       sed -i 's|ro.cm.device=hlte|ro.cm.device=hltetmo|g' /tmp/build.prop
+
+elif [[ $CMDLINE != "${CMDLINE/SM-N900W8}" ]]; #hltexx
+    then
+       sed -i 's|ro.build.fingerprint=xxxxxxxxxx|ro.build.fingerprint=samsung/hltevl/hltecan:4.3/JSS15J/N900W8VLUBMJ4:user/release-keys|g' /tmp/build.prop
+       sed -i 's|ro.build.description=xxxxxxxxxx|ro.build.description="hltevl-user 4.3 JSS15J N900W8VLUBMJ4 release-keys|g' /tmp/build.prop
+       sed -i 's|ro.product.model=hlte|ro.product.model=SM-N900W8|g' /tmp/build.prop
+        sed -i 's|ro.product.device=hlte|ro.product.device=hltecan|g' /tmp/build.prop
+       sed -i 's|ro.cm.device=hlte|ro.cm.device=hltecan|g' /tmp/build.prop
+elif [[ $CMDLINE != "${CMDLINE/SM-N900V}" ]]; #hltexx
+    then
+       sed -i 's|ro.build.fingerprint=xxxxxxxxxx|ro.build.fingerprint=Verizon/hltevzw/hltevzw:4.3/JSS15J/N900VOYUBMJ3:user/release-keys|g' /tmp/build.prop
+       sed -i 's|ro.build.description=xxxxxxxxxx|ro.build.description=hltevzw-user 4.3 JSS15J N900VOYUBMJ3 release-keys|g' /tmp/build.prop
+       sed -i 's|ro.product.model=hlte|ro.product.model=SM-N900W8|g' /tmp/build.prop
+        sed -i 's|ro.product.device=hlte|ro.product.device=hltevzw|g' /tmp/build.prop
+       sed -i 's|ro.cm.device=hlte|ro.cm.device=hltevzw|g' /tmp/build.prop
+
+fi
+
+mv /system/tmp/build.prop /system/build.prop
+chmod 644 /system/build.prop
+rm -r /system/tmp
