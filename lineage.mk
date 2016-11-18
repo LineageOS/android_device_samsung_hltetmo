1d132868ab7229fca2de9cc3630a07018e81ea03
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 17:54:08 +0800

    hltetmo: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- lineage.mk
+++ lineage.mk
@@ -15,9 +15,6 @@
 
 $(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
