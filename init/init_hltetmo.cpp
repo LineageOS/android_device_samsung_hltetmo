d2073da403a5a4241372d2e16b5c6cb8f5b62bbf
Bruno Martins <bgcngm@gmail.com>
2018-04-01 10:58:29 +0100

    hltetmo: libinit: Also override certain vendor build properties
    Change-Id: I2acce5df07619b4a737994b7d223f77b09425cb5

--- init/init_hltetmo.cpp
+++ init/init_hltetmo.cpp
@@ -84,31 +84,31 @@ void init_target_properties()
 
     if (bootloader.find("N900R4") == 0) {
         /* hlteusc */
-        property_override("ro.build.fingerprint", "samsung/hlteusc/hlteusc:5.0/LRX21V/N900R4TYUDPE2:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/hlteusc/hlteusc:5.0/LRX21V/N900R4TYUDPE2:user/release-keys");
         property_override("ro.build.description", "hlteusc-user 5.0 LRX21V N900R4TYUDPE2 release-keys");
-        property_override("ro.product.model", "SM-N900R4");
-        property_override("ro.product.device", "hlteusc");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N900R4");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "hlteusc");
         cdma_properties("U.S. Cellular", "311220", "10", "0", "usc");
     } else if (bootloader.find("N900T") == 0) {
         /* hltetmo */
-        property_override("ro.build.fingerprint", "samsung/hltetmo/hltetmo:5.0/LRX21V/N900TUVUFQD2:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/hltetmo/hltetmo:5.0/LRX21V/N900TUVUFQD2:user/release-keys");
         property_override("ro.build.description", "hltetmo-user 5.0 LRX21V N900TUVUFQD2 release-keys");
-        property_override("ro.product.model", "SM-N900T");
-        property_override("ro.product.device", "hltetmo");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N900T");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "hltetmo");
         gsm_properties("tmo");
     } else if (bootloader.find("N900V") == 0) {
         /* hltevzw */
-        property_override("ro.build.fingerprint", "Verizon/hltevzw/hltevzw:5.0/LRX21V/N900VVRSEPL1:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Verizon/hltevzw/hltevzw:5.0/LRX21V/N900VVRSEPL1:user/release-keys");
         property_override("ro.build.description", "hltevzw-user 5.0 LRX21V N900VVRSEPL1 release-keys");
-        property_override("ro.product.model", "SM-N900V");
-        property_override("ro.product.device", "hltevzw");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N900V");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "hltevzw");
         cdma_properties("Verizon", "311480", "10", "1", "vzw");
     } else if (bootloader.find("N900W8") == 0) {
         /* hltecan */
-        property_override("ro.build.fingerprint", "samsung/hltevl/hltecan:5.0/LRX21V/N900W8VLU2DPG1:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/hltevl/hltecan:5.0/LRX21V/N900W8VLU2DPG1:user/release-keys");
         property_override("ro.build.description", "hltevl-user 5.0 LRX21V N900W8VLU2DPG1 release-keys");
-        property_override("ro.product.model", "SM-N900W8");
-        property_override("ro.product.device", "hltecan");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N900W8");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "hltecan");
         gsm_properties("can");
     } else {
         gsm_properties("tmo");
