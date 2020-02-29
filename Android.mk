1a682f205afd3cf74a1072f724f335e5ad6b4dd2
Kevin F. Haggerty <haggertk@lineageos.org>
2020-02-29 13:08:54 -0700

    hltetmo: Correct top-level Android.mk guard
    Change-Id: I66001f76880f58163de32b49bb87b2924f59b1cf

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The Android Open-Source Project
+#               2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +17,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),hlte)
+ifeq ($(TARGET_DEVICE),hltetmo)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
